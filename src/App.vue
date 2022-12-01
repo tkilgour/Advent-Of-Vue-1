d8b297cee78e781cd3e1b773f95a55e7c73cf454
Thomas Kilgour <thomas@kilgour.co>
2022-12-01 11:32:06 -0500

    implement debounced search bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
+import { useDebounceFn } from "@vueuse/core";
 
 const searchTerm = ref('')
+const products = ref([])
+const loading = ref(false)
+const noResults = ref(false)
 
-const findProducts = async term => {}
+const findProducts = useDebounceFn(async term => {
+  noResults.value = false
+
+  if (term.length) {
+    try {
+      loading.value = true
+      const response = await fetch(`https://dummyjson.com/products/search?q=${term}`)
+      const result = await response.json()
+      products.value = result.products
+      noResults.value = !products.value.length
+      loading.value = false
+    } catch (error) {
+      loading.value = false
+      alert('Whoops, something went wrong!')
+      searchTerm.value = ''
+    }
+  } else {
+    products.value = []
+  }
+}, 300)
 
 watch(searchTerm, newTerm => findProducts(newTerm))
 </script>
@@ -12,8 +35,10 @@ watch(searchTerm, newTerm => findProducts(newTerm))
   <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
-    <ul class="list-disc">
-      <li>Display suggestions here</li>
+    <p v-if="loading">Loading...</p>
+    <ul v-else class="list-disc">
+      <li v-for="product in products" :key="product.id">{{ product.title }}</li>
     </ul>
+    <p v-if="noResults">No results found.</p>
   </div>
 </template>
